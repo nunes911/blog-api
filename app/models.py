a006cd85c1978a3abeb290d5125deedd0f9892f1
nunes911 <marcus.live.nunes@gmail.com>
2022-03-27 18:49:31 -0300

    Criando models

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
