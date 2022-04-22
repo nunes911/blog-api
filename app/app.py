ea326c1bb876335ae8f410eaaed1c9e9c44f5c8f
nunes911 <marcus.live.nunes@gmail.com>
2022-04-22 15:07:56 -0300

    Aplicando reconhecimento de banco

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 import os
 from models import db
+from sqlalchemy_utils.functions import database_exists
 from api import api
 from views.user import user
 from views.post import post
@@ -16,14 +17,20 @@ def create_app():
     api.add_namespace(user)
     api.add_namespace(post)
 
-    # SQLite
+    # Configuração SQLite
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # Configuração de Ambiente
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['DEBUG'] = True
     app.config['USE_RELOADER'] = True
 
     db.init_app(app)
+
+    # Se o banco não existir, levanta o banco automaticamente
+    with app.app_context():
+        if not database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
+            db.create_all(app=app)            
 
     return app
